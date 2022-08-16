163f7bb63b81ab8f2d602226a087d1d54b3a2632
SHUBHAM <shubhamtiwary50@gmail.com>
2022-08-16 15:28:04 +0530

    Created Furniture class, declared attributes and created getter and setters

---
+++ src/com/furniture/FurnitureItem.java
@@ -0,0 +1,59 @@
+package com.furniture;
+
+public class FurnitureItem {
+    private int furnitureCode;
+    private String furnitureType;
+    private String gradeOfFurniture;
+    private String colour;
+    private String furnitureUsage;
+    private double price;
+
+    public int getFurnitureCode() {
+        return furnitureCode;
+    }
+
+    public void setFurnitureCode(int furnitureCode) {
+        this.furnitureCode = furnitureCode;
+    }
+
+    public String getFurnitureType() {
+        return furnitureType;
+    }
+
+    public void setFurnitureType(String furnitureType) {
+        this.furnitureType = furnitureType;
+    }
+
+    public String getGradeOfFurniture() {
+        return gradeOfFurniture;
+    }
+
+    public void setGradeOfFurniture(String gradeOfFurniture) {
+        this.gradeOfFurniture = gradeOfFurniture;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getFurnitureUsage() {
+        return furnitureUsage;
+    }
+
+    public void setFurnitureUsage(String furnitureUsage) {
+        this.furnitureUsage = furnitureUsage;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
